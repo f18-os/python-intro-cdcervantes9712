bda2d1f92dda1c477ee982d87ec80a61c6b9e1bc
Carlos Cervantes <31900188+cdcervantes9712@users.noreply.github.com>
2018-09-03 22:19:23 -0700

    Add files via upload

--- wordCount.py
+++ wordCount.py
@@ -1,11 +1,16 @@
-import sys
+import sys, re, string, timeit
 
-f = open(sys.argv[1], 'r')
+fsource = open(sys.argv[1], 'r')
+data = fsource.read()
 
-data = f.read()
-word_list = data.split(" ")
+for char in '-.,\n':
+    data = data.replace(char, ' ')
+data = data.lower()
 
+word_list = data.split()
 word_list[:] = (value for value in word_list if value != '\t')
+
+fsource.close()
 
 wordCounter = {}
 
@@ -13,8 +18,15 @@ for word in word_list:
     wordCounter[word] = wordCounter.get(word, 0) + 1
 
 word_freq = []
+
 for key, value in wordCounter.items():
     word_freq.append((value, key))
 
 word_freq.sort(reverse=True)
-print(word_freq)
+
+ftarg = open(sys.argv[2], 'w+')
+
+for word in word_freq:
+    ftarg.write(word[1] + ", " + str(word[0]) + "\n")
+
+ftarg.close()
